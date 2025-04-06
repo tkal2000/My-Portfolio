1d95cc813d92d0eb35f96572c3a2e65a611d95ce
Kalpana <s22010441@ousl.lk>
2025-04-06 14:27:34 +0530

    Upload all files

---
+++ myscript.js
@@ -0,0 +1,11 @@
+// Position animation
+var typed = new Typed('.typing_text', {
+    strings: ['Frontend Developer', 'Full Stack Developer', 'UI/UX Designer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+
+
